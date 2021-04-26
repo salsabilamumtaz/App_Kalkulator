ed0ce8c83798473d810e1dcc4fc938199d1c4d53
salsabilamumtaz <68579789+salsabilamumtaz@users.noreply.github.com>
2021-04-26 11:12:00 +0700

    Add files via upload

---
+++ appkalkulator/appkalkulator/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace appkalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_bil1 = new System.Windows.Forms.TextBox();
+            this.txt_hasil = new System.Windows.Forms.TextBox();
+            this.txt_bil2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Angka 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasil";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Angka 2";
+            // 
+            // txt_bil1
+            // 
+            this.txt_bil1.Location = new System.Drawing.Point(79, 25);
+            this.txt_bil1.Name = "txt_bil1";
+            this.txt_bil1.Size = new System.Drawing.Size(100, 20);
+            this.txt_bil1.TabIndex = 3;
+            // 
+            // txt_hasil
+            // 
+            this.txt_hasil.Location = new System.Drawing.Point(79, 90);
+            this.txt_hasil.Name = "txt_hasil";
+            this.txt_hasil.Size = new System.Drawing.Size(100, 20);
+            this.txt_hasil.TabIndex = 4;
+            // 
+            // txt_bil2
+            // 
+            this.txt_bil2.Location = new System.Drawing.Point(79, 57);
+            this.txt_bil2.Name = "txt_bil2";
+            this.txt_bil2.Size = new System.Drawing.Size(100, 20);
+            this.txt_bil2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 36);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 36);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "x";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(253, 130);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 36);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 283);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_bil2);
+            this.Controls.Add(this.txt_hasil);
+            this.Controls.Add(this.txt_bil1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "kalkulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_bil1;
+        private System.Windows.Forms.TextBox txt_hasil;
+        private System.Windows.Forms.TextBox txt_bil2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ appkalkulator/appkalkulator/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace appkalkulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int bil1, bil2, hasil;
+            bil1 = int.Parse(txt_bil1.Text);
+            bil2 = int.Parse(txt_bil2.Text);
+            hasil = bil1 + bil2;
+            txt_hasil.Text = hasil.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int bil1, bil2, hasil;
+            bil1 = int.Parse(txt_bil1.Text);
+            bil2 = int.Parse(txt_bil2.Text);
+            hasil = bil1 - bil2;
+            txt_hasil.Text = hasil.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int bil1, bil2, hasil;
+            bil1 = int.Parse(txt_bil1.Text);
+            bil2 = int.Parse(txt_bil2.Text);
+            hasil = bil1 * bil2;
+            txt_hasil.Text = hasil.ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int bil1 ;
+            double bil2, hasil;
+            bil1 = int.Parse(txt_bil1.Text);
+            bil2 = int.Parse(txt_bil2.Text);
+            hasil = bil1 / bil2;
+            txt_hasil.Text = hasil.ToString();
+
+        }
+    }
+}
